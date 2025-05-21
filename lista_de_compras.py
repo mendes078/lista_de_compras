adcb2c7d053e08122a772ec2fe3d42d7a7ce0288
kai <oincrivelkaike@gmail.com>
2025-05-21 13:24:05 -0300

    2 commit

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -7,4 +7,8 @@ while True:
         chave = str(input("Informe o nome do produto: ")).strip()
         valor = float(input("Informe o preço do produto: "))
     except ValueError:
-        print("O tipo de valor está incorreto. Tente novamente.")
+        valor = float(input("Informe o preço do produto: "))
+    break
+
+print(chave)
+print(valor)
