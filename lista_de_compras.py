389c51593c71e37c5f448c989d01464d91e06d9e
kai <oincrivelkaike@gmail.com>
2025-05-21 20:19:27 -0300

    3 commit

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -3,12 +3,17 @@ import pandas as pd
 dados = {}
 
 while True:
-    try:
-        chave = str(input("Informe o nome do produto: ")).strip()
-        valor = float(input("Informe o preço do produto: "))
-    except ValueError:
-        valor = float(input("Informe o preço do produto: "))
-    break
+    chave = str(input("Informe o nome do produto: ")).strip()
 
-print(chave)
-print(valor)
+    try:
+        valor = float(input("Informe o preço do produto: R$"))
+    except:
+        while type(valor) != float:
+            valor = float(input("Informe o preço do produto: R$"))
+    dados[chave] = valor
+
+    continuar = str(input("Continuar: "))
+    if continuar in "Nn":
+        break
+
+print(dados)
