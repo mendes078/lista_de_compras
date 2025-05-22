2c746b1e122f09177d8690888e247948a452a533
kai <oincrivelkaike@gmail.com>
2025-05-22 11:34:30 -0300

    4 commit

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -4,16 +4,21 @@ dados = {}
 
 while True:
     chave = str(input("Informe o nome do produto: ")).strip()
-
     try:
         valor = float(input("Informe o preço do produto: R$"))
-    except:
-        while type(valor) != float:
-            valor = float(input("Informe o preço do produto: R$"))
-    dados[chave] = valor
+    except ValueError:
+        print("O tipo de valor inserido não foi aceito!")
+        valor = float(input("Informe o preço do produto corretamente: R$"))
 
-    continuar = str(input("Continuar: "))
+    dados[chave] = valor
+    print(f"O item {chave} custando R${valor} reais foi adicionado a lista com sucesso!")
+
+    continuar = str(input("Deseja continuar? Digite S (Sim) ou N (Não): ")).strip()[0]
     if continuar in "Nn":
         break
 
+    while continuar not in "SsNn":
+        continuar = str(input("O valor inserido está incorreto! Digite S (Sim) ou N (Não): ")).strip()[0]
+        if continuar == "Nn":
+            break
 print(dados)
